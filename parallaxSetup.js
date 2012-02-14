014fe6b6179ca68805d98bbf477a01a3190f974f
Cody Sehl <cody.sehl@gmail.com>
2012-02-14 15:43:11 -0600

    Inner page layout finished. Created prev/next buttons. Using parallax.js. Finished front page. wokring on second page

---
+++ parallaxSetup.js
@@ -0,0 +1,70 @@
+/** PARALLAX CODE **/
+
+$(document).ready(function(){
+  parallax.add($("#title"))
+    .add($("#gridSize"))
+    .add($("#options"))
+    .add($("#importantLettersAutoGen"))
+    .add($("#importantLettersManual"))
+    .add($("#overview"))
+    .add($("#individualLetter"))
+    .add($("#download"));
+    
+  parallax.background = $("body");
+    
+  //Set up the triggers for the arrow keys
+	$(document).keydown(function(e){
+		if (e.keyCode == 37 && typeof leftKey === 'function') { 
+			leftKey();
+		} else if(e.keyCode == 38 && typeof topKey === 'function') { 
+			topKey();
+		} else if(e.keyCode == 39 && typeof rightKey === 'function') { 
+			rightKey();
+		} else if(e.keyCode == 40 && typeof bottomKey === 'function') { 
+			bottomKey();
+		}
+	});
+  
+  //Clears each page navigation on load
+	parallax.preload = function(){
+		rightKey = leftKey = topKey = bottomKey = "";
+		$(".control").hide();
+	};
+    
+  parallax.title.onload=function(){
+      setRight("gridSize");
+    };
+  parallax.gridSize.onload=function(){
+    setLeft("title");
+    setRight("options","Options");
+  };
+  parallax.options.onload=function(){
+    setLeft("gridSize");
+    //~ setLeft("importantLettersAutoGen","Let the computer do the work");
+    //~ setRight("importantLettersManual","Do the work yourself");
+  };
+  
+  //Sets the correct triggers for the arrows, plus arrow keys
+	function setRight(page){
+		$("#nextControl").show().unbind('click').click(function(){
+			parallax[page].right();
+		});
+		rightKey = function(){ 
+			parallax[page].right(); 
+		};
+	}
+	
+	function setLeft(page){
+		$("#prevControl").show().unbind('click').click(function(){
+			parallax[page].left();
+		});
+		leftKey = function(){ 
+			parallax[page].left(); 
+		};
+	}
+	
+  $(".control").hide();
+    parallax.gridSize.show();
+});
+
+
